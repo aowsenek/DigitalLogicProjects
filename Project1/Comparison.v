8420ba38c5fc22e9e950b121dae18b9d68d79558
Alexander Curtiss <apccurtiss@gmail.com>
2017-03-02 14:58:04 -0700

    Got comparison working

--- Project1/Comparison.v
+++ Project1/Comparison.v
@@ -1,3 +1,20 @@
 module Comparison(input [1:0] mode, input [3:0]x, input [3:0]y, output [3:0]o);
-	assign o = x;
+	wire yg, xg;
+	wire [3:0] max;
+	less_than(x, y, yg);
+	less_than(y, x, xg);
+	Multiplexer(xg, y, x, 0, 0, max); // last two not used
+	Multiplexer(mode, ~(xg|yg), xg, yg, max, o);
+endmodule
+
+module less_than(input [3:0]x, input[3:0]y, output o);
+	reg [4:0] g;
+	integer i;
+	always @ (x or y) begin
+		for (i = 0; i < 4; i = i + 1) begin
+			g[i+1] = ((~(x[i]^y[i]))&g[i])|((x[i]^y[i])&y[i]);
+		end
+		g[0] = 0;
+	end
+	assign o = g[4];
 endmodule
--- Project1/Project1_top.v
+++ Project1/Project1_top.v
@@ -1,10 +1,13 @@
-module Project1_top(input [1:0]buttons, input [9:0]switches, input clock, output [9:0]LEDs, output [6:0]Hex1In, output [6:0]Hex2In, output [6:0]Hex3In, output [6:0]Hex4In);
+module Project1_top(input [1:0]buttons, input [9:0]switches, input clock, output [9:0]LEDs, output [6:0]SS0, output [6:0]SS1, output [6:0]SS2, output [6:0]SS3, output [6:0]SS4, output [6:0]SS5);
 	wire [7:0] num1, num2;
 	wire [7:0] ao, lo, co, mo;
-	SevenSegment Mode(switches[9:8], Hex4In);
-	assign Hex3In = 7'b1111111; // turn it off for now
-	SevenSegment Hex1(num1[7:4], Hex2In); // placeholder
-	SevenSegment Hex2(num1[3:0], Hex1In); // placeholder
+	SevenSegment I0(switches[7:4], SS0);
+	SevenSegment I1(switches[3:0], SS1);
+	SevenSegment I2(buttons, SS2);
+//	assign SS2 = 7'b1111111;
+	assign SS3 = 7'b1111111;
+	SevenSegment O0(num1[7:4], SS4);
+	SevenSegment O1(num1[3:0], SS5);
 	Arithmetic a(switches[9:8], switches[7:0], ao, LEDs[9]);
    Logical l(switches[9:8], switches[7:0], lo);
 	Comparison c(switches[9:8], switches[7:4], switches[3:0], co);
