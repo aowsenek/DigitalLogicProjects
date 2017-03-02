5f727d584c04f84940a2251e8adce58174ab54cf
Alexander Curtiss <apccurtiss@gmail.com>
2017-03-02 10:48:07 -0700

    Subtraction works!

--- Project1/Arithmetic.v
+++ Project1/Arithmetic.v
@@ -1,8 +1,8 @@
 module Arithmetic(input [1:0] mode, input [7:0] i, output [7:0] o, output carry);
 	wire [7:0] oa, os, om, od;
 	wire ca, cs, cm;
-	full_add a(i[3:0], i[7:4], oa, ca);
-	subtract s(i[3:0], i[7:4], os, cs);
+	full_add a(i[7:4], i[3:0], oa, ca);
+	full_subtract s(i[7:4], i[3:0], os, cs);
 	mult_by_2 m(i[7:0], om, cm);
 	div_by_2 d(i[7:0], od);
 	
@@ -20,13 +20,22 @@ module div_by_2(input [7:0] i, output [7:0] o);
 	assign o[7] = 0;
 endmodule
 
-module subtract(x, y, s, cout);
+module full_subtract(x, y, s, cout);
 	parameter n = 8;
 	input [n-1:0]x;
 	input [n-1:0]y;
-	output [n-1:0]s;
+	output reg [n-1:0]s;
 	output cout;
-	assign s = 0;
+	reg [n:0]c;
+	integer i;
+	always @ (x or y) begin
+		for (i = 0; i < n; i = i + 1) begin
+			c[i+1] = (~x[i]&y[i])|(c[i]&~(x[i]&~y[i]));
+			s[i] = x[i] ^ y[i] ^ c[i];
+		end
+		c[0] = 0;
+	end
+	assign cout = c[n];
 endmodule
 
 
@@ -49,7 +58,7 @@ module full_add(x, y, s, cout);
   generate
   genvar i;
     for(i = 0; i < n; i = i + 1)
-    begin: genStages
+    begin: gen_stages
       add stage(x[i], y[i], c[i], s[i], c[i+1]);
     end
   endgenerate
